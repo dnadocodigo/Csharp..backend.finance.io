c1dd07d0c737c65a9b964705d044f80775586225
Paulo Galdino <dnadevnet@gmail.com>
2023-06-11 23:44:17 -0300

    Criado as classes Category e Expense no projeto Domain

---
+++ src/2-Domains/Domain/Entities/Systems/Categories/Category.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Systems.Expenses.Systems;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Systems.Categories
+{
+    public class Category
+    {
+        [ForeignKey("SystemsExpense")]
+        [Column(Order = 1)]
+        public int SystemId { get; set; }
+
+        public SystemExpense SystemsExpense { get; set; }
+    }
+}
---
+++ src/2-Domains/Domain/Entities/Systems/Expenses/Expense.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities.Systems.Categories;
+using Domain.Enums.Types.Systems;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Systems.Expenses
+{
+    public class Expense : Base
+    {
+        public decimal Value { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public DateTime ExpensesRegister { get; set; }
+        public DateTime ExpenseUpdate { get; set; }
+        public DateTime PayDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsPayedExpense { get; set; }
+        public bool IsOverDueExpense { get; set; }
+
+        //ForeignKey
+        [ForeignKey("Category")]
+        [Column(Order = 1)]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
