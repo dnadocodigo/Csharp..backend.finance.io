96560294b0ed5d0c365396405c9140535551601e
Paulo Galdino <dnadevnet@gmail.com>
2023-06-11 23:12:50 -0300

    Criado a class Base no projeto DOmain

---
+++ src/2-Domains/Domain/Entities/Base.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domain.Entities
+{
+    public class Base : Notifier
+    {
+       
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
