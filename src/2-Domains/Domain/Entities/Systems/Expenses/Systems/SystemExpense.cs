3ff556cd39d02bc4a9d343a38633cb6b646744bb
Paulo Galdino <dnadevnet@gmail.com>
2023-06-11 23:36:22 -0300

    criado a classe SystemExpense no projeto DOmain

---
+++ src/2-Domains/Domain/Entities/Systems/Expenses/Systems/SystemExpense.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities.Systems.Expenses.Systems
+{
+    public class SystemExpense : Base
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int MonthlyBookClose { get; set; }
+        public bool GenerateExpensesCopy { get; set; }
+        public int MonthCopy { get; set; }
+        public int YearCopy { get; set; }
+    }
+}
