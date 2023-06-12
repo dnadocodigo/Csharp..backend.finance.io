3d1aef5889866fbd330f0eb893a295551092dc31
Paulo Galdino <dnadevnet@gmail.com>
2023-06-11 23:51:19 -0300

    Creste class SystemUser on project Domain

---
+++ src/2-Domains/Domain/Entities/Systems/Users/SystemUser.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.Systems.Expenses.Systems;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Systems.Users
+{
+    public class SystemUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public bool Administrator { get; set; }
+        public bool ActualSytem { get; set; }
+
+        //Foreign Key
+        [ForeignKey("SystemExpense")]
+        [Column(Order = 1)]
+        public int SystemId { get; set; }
+        public virtual SystemExpense SystemExpense { get; set; }
+    }
+}
